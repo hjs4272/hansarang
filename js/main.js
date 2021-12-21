eb071537effb72185875fdfa7d0d9fa331e5af0e
hjs4272 <93746830+hjs4272@users.noreply.github.com>
2021-12-22 00:43:49 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -5,8 +5,15 @@ $(function(){
     grabCursor: true,
   });
 
-
-
 });
+
+// 햄버거 사이드 쇼업 메뉴
+function openNav(){
+  $("#leftMenu").addClass("showLeftMenu");
+}
+
+function closeNav(){
+  $("#leftMenu").removeClass("showLeftMenu")
+}
 
     

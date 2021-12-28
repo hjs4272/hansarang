cb8ccebd5cc1fbb74e7bdb4424ec04b7b5338b32
hjs4272 <93746830+hjs4272@users.noreply.github.com>
2021-12-28 11:24:10 +0900

    Add files via upload

--- js/alert2.js
+++ js/alert2.js
@@ -1,4 +1,4 @@
 const 상담신청준비중2 = document.querySelector("#form-submit--alert");
-상담신청준비중2.addEventListener('click', function(){
-    alert("서비스 준비중입니다.");
-})
+상담신청준비중2.addEventListener("click", function () {
+  alert("서비스 준비중입니다.");
+});
--- js/main.js
+++ js/main.js
@@ -1,42 +1,33 @@
 // pc 왼쪽 하단 슬라이드
-$(function(){
-    
+$(function () {
   const swiper = new Swiper(".mySwiper", {
     effect: "cards",
     grabCursor: true,
   });
-
 });
 
 // 모바일 햄버거 사이드 쇼업 메뉴 (왼쪽)
-function openNav(){
+function openNav() {
   $("#leftMenu").addClass("showLeftMenu");
 }
 
-function closeNav(){
-  $("#leftMenu").removeClass("showLeftMenu")
+function closeNav() {
+  $("#leftMenu").removeClass("showLeftMenu");
 }
 
 // 모바일 오른쪽 사이드 쇼업 메뉴
-function openNav2(){
+function openNav2() {
   $("#rightMenu").addClass("showRightMenu");
 }
 
-function closeNav2(){
-  $("#rightMenu").removeClass("showRightMenu")
+function closeNav2() {
+  $("#rightMenu").removeClass("showRightMenu");
 }
-
-
-
-
-
-
 
 //자바스크립트
 // 보안로그인 색 클릭 변경
 const 자물쇠아이콘 = document.querySelector(".fa-lock");
 
-자물쇠아이콘.addEventListener('click', function() {
-자물쇠아이콘.classList.toggle('fa-lock--orange');
+자물쇠아이콘.addEventListener("click", function () {
+  자물쇠아이콘.classList.toggle("fa-lock--orange");
 });
-
